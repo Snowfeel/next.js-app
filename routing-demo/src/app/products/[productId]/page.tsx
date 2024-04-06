59844367caed394df483f37baf192768b82077c8
Snowfeel <b.siri.th@outlook.com>
2024-04-06 15:21:30 +0700

    add generate metadata

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 1000);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetail({ params }: Props) {
   return (
     <div>
       <h1>Product Detail {params.productId}</h1>
